724d253245d54a72c6fa8c9d907bb52c6e4f94d7
Sunil Joshi <joshisunil2601@gmail.com>
2022-11-23 13:32:49 +0530

    Button

--- src/Heading/Button.tsx
+++ src/Heading/Button.tsx
@@ -1,14 +1,36 @@
-import React, { Component } from "react";
+import React, { ButtonHTMLAttributes, Component } from "react";
 type buttonProps = {
   children: string;
-};
+  them?: "highlight" | "secondary" | "primary";
+} & ButtonHTMLAttributes<HTMLButtonElement>;
 
 export default class Button extends Component<buttonProps> {
   render() {
+    let themClass =
+      "text-white rounded-md bg-yellow-500 border-transparent  hover:bg-yellow-600 ";
+
+    let radiusClass = "";
+    if (this.props.them === "highlight") {
+      radiusClass = " mt-2 rounded-full";
+    }
+    if (this.props.them === "secondary") {
+      themClass =
+        "text-gray-900 bg-white  border-gray-300 hover:bg-gray-600 rounded-md";
+    }
     return (
-      <button className="px-4 py-2  bg-yellow-500  text-white disabled:bg-gray-400">
+      <button
+        className={
+          "px-4 py-2   border  disabled:bg-gray-400  " +
+          themClass +
+          " " +
+          radiusClass
+        }
+      >
         {this.props.children}
       </button>
     );
   }
+  defaultProps = {
+    them: "primary",
+  };
 }
