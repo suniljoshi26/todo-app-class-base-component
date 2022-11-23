b7bb54d1ad08a9481fa18c1774730850a50049af
Sunil Joshi <joshisunil2601@gmail.com>
2022-11-23 15:57:57 +0530

    form

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import Card from "./Heading/Form/Card";
 import TodoPage from "./TodoPage";
 
 function App() {
   return (
     <div className="">
       <TodoPage />
+      <Card />
     </div>
   );
 }
---
+++ src/Heading/Form/Card.tsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export default class Card extends Component {
+  render() {
+    return (
+      <div className="p-4 rounded-md border border-gray-300 shadow-md"></div>
+    );
+  }
+}
