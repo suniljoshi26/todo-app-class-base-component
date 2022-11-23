c27827250f257e2a006adf943ba4405c428eaf05
Sunil Joshi <joshisunil2601@gmail.com>
2022-11-23 16:32:36 +0530

    form complete

--- src/Heading/Form/Input.tsx
+++ src/Heading/Form/Input.tsx
@@ -6,7 +6,7 @@ export default class Input extends Component {
       <div>
         <input
           placeholder="Write an article about XState"
-          className="border border-gray-300 rounded-md px-4 py-2 w-72"
+          className="border border-gray-300 rounded-md mt-4 px-4 py-2 w-72"
         />
       </div>
     );
