cc7a23a93cd9902cbf58b3f1ed691ed71a975799
Sunil Joshi <joshisunil2601@gmail.com>
2022-11-26 15:55:35 +0530

    complete

--- src/Checkbox.tsx
+++ src/Checkbox.tsx
@@ -4,6 +4,7 @@ export default class Checkbox extends Component<CheckboxP> {
   render() {
     return (
       <input
+        className="w-4 h-4"
         type="checkbox"
         checked={this.props.checked}
         onChange={this.props.onChange}
